2e0f7827b9fa27304ed924276d1c83642b285451
frosti-4 <jopa.popa.78@mail.ru>
2026-07-01 00:02:32 +0300

    перешел на alacritty

--- swaync/.config/swaync/caffeine.sh
+++ swaync/.config/swaync/caffeine.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Проверяем, запущен ли уже процесс ингибитора
-if pgrep -f "systemd-inhibit.*sleep infinity" > /dev/null; then
-    # Если запущен - убиваем его (разрешаем сон)
-    pkill -f "sleep infinity"
-    notify-send "Caffeine" "Caffeine disabled" -i notifications-applet
+if pgrep -f "systemd-inhibit.*sleep infinity" >/dev/null; then
+  pkill -f "sleep infinity"
+  notify-send "Caffeine" "Caffeine disabled" -i notifications-applet
 else
-    # Если не запущен - блокируем переход в состояние idle
-    systemd-inhibit --what=idle --who="SwayNC" --why="Manual toggle" --mode=block sleep infinity &
-    notify-send "Caffeine" "Caffeine (PC stays awake)" -i nightly
+  systemd-inhibit --what=idle --who="SwayNC" --why="Manual toggle" --mode=block sleep infinity &
+  notify-send "Caffeine" "Caffeine (PC stays awake)" -i nightly
 fi
